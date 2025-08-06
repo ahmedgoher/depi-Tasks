b7382a073a713e8d6840a93b727fb4bd06771b67
Ahmed A Goher <68221996+ahmedgoher@users.noreply.github.com>
2025-08-06 17:19:34 +0300

    Add files via upload

---
+++ BuiltSystem/BuiltSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BuiltSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ BuiltSystem/BuiltSystem/users.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuiltSystem
+{
+    internal class users
+    {
+        int _userID;
+        string _userName;
+        string _password;
+        
+        public int UserID {
+            get { return _userID; } 
+            set {
+                
+                _userID = value;
+            }
+        }
+        public string UserName {
+            get { return _userName; }
+            set {
+                if (string.IsNullOrEmpty(value))
+                    Console.WriteLine("Invalied");
+                else
+                    _userName = value;
+            } 
+        }
+        public string Password {
+            get { return _password; } 
+            set {
+                if (string.IsNullOrEmpty(value))
+                    Console.WriteLine("Invalied");
+                else
+                    _password = value;
+            }
+        }
+
+    }
+}
